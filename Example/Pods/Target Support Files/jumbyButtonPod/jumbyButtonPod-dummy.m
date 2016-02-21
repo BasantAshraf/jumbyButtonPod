26d03825262eb0bcdd9fae244b85b37d920e79c3
Basant Ashraf <bassant.ashraf@ibtikar.net.sa>
2016-02-21 12:31:29 +0200

    Initial Commit

---
+++ Example/Pods/Target Support Files/jumbyButtonPod/jumbyButtonPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_jumbyButtonPod : NSObject
+@end
+@implementation PodsDummy_jumbyButtonPod
+@end
---
+++ Example/Pods/Target Support Files/jumbyButtonPod/jumbyButtonPod-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double jumbyButtonPodVersionNumber;
+FOUNDATION_EXPORT const unsigned char jumbyButtonPodVersionString[];
+
